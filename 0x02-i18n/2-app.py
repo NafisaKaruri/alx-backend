05af25e20006f02ee3989fe101076507aeb5ac75
NafisaKaruri <nafeesahassan016@gmail.com>
2024-11-05 21:54:42 +0000

    fixed typo

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,7 +2,7 @@
 """
 Flask app with dynamic locale detection based on request headers.
 """
-from flask imporrt Flask, render_template, request
+from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 app = Flask(__name__)
