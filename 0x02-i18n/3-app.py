ca4fb536d17d67d3674c087fdb95f0d2130386e7
NafisaKaruri <nafeesahassan016@gmail.com>
2024-11-05 22:28:09 +0000

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Flask app with dynamic locale detection based on request headers.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """Cofiguration class for the app."""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Render the home page."""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
